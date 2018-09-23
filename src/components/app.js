ea3b629021c410e3bda923a3dbfe772d72630f2e
macbookair <macbookair@macbookairs-MacBook-Air.local>
2018-09-23 15:51:20 -0700

    added birhtday countdown title

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,9 @@ export default class App extends Component {
   render() {
     return (
       <div className="grid">
+          <h1 className="grid__title">Idris's Birthday Countdown</h1>
+
+
         <div className="grid__skew-dark-one"></div>
         <div className="grid__skew-dark-two"></div>
         <div className="grid__skew-dark-three"></div>
