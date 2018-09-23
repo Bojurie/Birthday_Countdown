9f76e1a5cdc6717fce104eac94e1135c7cb14e7c
macbookair <macbookair@macbookairs-MacBook-Air.local>
2018-09-23 16:46:34 -0700

    installed moment.js, react-datepicker, and placed picker component on grid with datepicker within it.

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Picker from './picker';
 
 export default class App extends Component {
   render() {
@@ -14,6 +15,8 @@ export default class App extends Component {
         <div className="grid__skew-light-one"></div>
         <div className="grid__skew-light-two"></div>
         <div className="grid__skew-light-three-box"></div>
+
+       <picker/>
       </div>
     );
   }
